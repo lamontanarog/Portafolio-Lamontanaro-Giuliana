f1659ef3472571b2cecb0024741ffa197fe561f9
Giuliana lamontanaro <giulianalamontanaro@gmail.com>
2024-02-22 17:27:55 -0300

    Pusheo para pull Request

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-info text-body">
+      <nav className="navbar navbar-expand-lg bg-white text-body mt-1">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">FullStack developer</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,10 +16,10 @@ function App() {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Mis Proyectos</a>
+                <a className="nav-link" aria-current="page" href="/proyectos">Mis Proyectos</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/sobremi">Sobre mi</a>
+                <a className="nav-link" href="/">Sobre mi</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="/misconocimientos">Mis conocimientos</a>
@@ -32,8 +32,8 @@ function App() {
       </div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MisProyectos/>}></Route>
-          <Route path="/sobremi" element={<Sobremi/>}></Route>
+          <Route path="/" element={<Sobremi/>}></Route>
+          <Route path="/proyectos" element={<MisProyectos/>}></Route>
           <Route path="/misconocimientos" element={<Misconocimientos/>}></Route>
         </Routes>
 
--- src/Components/Proyectos.jsx
+++ src/Components/Proyectos.jsx
@@ -1,6 +1,7 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Swal from "sweetalert2";
+import './proyectos.css'
 function MisProyectos() {
   return (
     <>
