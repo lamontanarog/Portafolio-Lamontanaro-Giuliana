af318514af3f12f6798ef1a55e709289aae7575f
Giuliana Lamontanaro <glamontanaro@gmail.com>
2024-02-22 22:27:09 -0300

    carrouselsobremi

--- src/Components/Proyectos.jsx
+++ src/Components/Proyectos.jsx
@@ -1,7 +1,7 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Swal from "sweetalert2";
-import './proyectos.css'
+import "./proyectos.css";
 function MisProyectos() {
   return (
     <>
@@ -13,9 +13,166 @@ function MisProyectos() {
               <Card.Body>
                 <Card.Title>Control de gastos</Card.Title>
                 <Card.Text>
-                  Proyecto realizado con Reactjs, css, boostrap, firebase
+                  Proyecto realizado con Reactjs, Css, Bootstrap, Firebase.
                 </Card.Text>
-                <Button className="mx-4"
+                <Button
+                  className="mx-4"
+                  onClick={() => {
+                    Swal.fire({
+                      title:
+                        "El proyecto consiste en una aplicación para gestionar los gastos, ingresos y el balance general de los usuarios. Ha sido desarrollado utilizando ReactJS, haciendo uso de hooks, context y diversas librerías para una mejor organización y funcionalidad. Además, se integra Firebase para gestionar la autenticación y el control de usuarios, brindando así una experiencia segura y personalizada.",
+                      width: 700,
+                      padding: "4em",
+                      color: "#fff",
+                      background: "#aecb9d ",
+                      backdrop: `
+                      rgba(0,0,123,0.4)
+                      url("../gifs/controlgastos.gif")
+                      left bottom
+                      no-repeat
+                    `,
+                    });
+                  }}
+                >
+                  Ver info
+                </Button>
+                &nbsp;
+                <Button variant="primary">
+                  <a
+                    className="text-light text-decoration-none"
+                    href="https://control-gastos-gold.vercel.app/"
+                  >
+                    Ver proyecto
+                  </a>
+                </Button>
+                &nbsp;
+                <Button className="mx- mt-1" variant="primary">
+                  <a
+                    className="text-light text-decoration-none"
+                    href="https://github.com/lamontanarog/controlGastos"
+                  >
+                    Ver Repositorio
+                  </a>
+                </Button>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col">
+            <Card className="text-center" style={{ width: "19rem" }}>
+              <Card.Img
+                variant="top"
+                src="../icons/Splendora_Cosmetics-logos.jpeg"
+              />
+              <Card.Body>
+                <Card.Title>Splendora Cosmetics</Card.Title>
+                <Card.Text>
+                  Proyecto realizado con HTML, Css, Bootstrap, JavaScript, Apis
+                </Card.Text>
+                <Button
+                  className="mx-4"
+                  onClick={() => {
+                    Swal.fire({
+                      title:
+                        "El proyecto es un E-commer dedicado a la venta de productos cosméticos. La plataforma cuenta con un carrito de compras incorporado que permite a los usuarios seleccionar y comprar productos de manera sencilla. Además, la aplicación consume una API que proporciona el valor actual del dólar para realizar la conversión de precios de los productos a pesos, brindando así una experiencia de compra más transparente para los usuarios.",
+                      width: 700,
+                      padding: "4em",
+                      color: "#fff",
+                      background: "url(../public/img/Backgroundcolors.avif) ",
+                      backdrop: `
+                      rgba(0,0,123,0.4)
+                      url("../public/gifs/SplendoraGif.gif")
+                      left bottom
+                      no-repeat
+                    `,
+                    });
+                  }}
+                >
+                  Ver info
+                </Button>
+                &nbsp;
+                <Button variant="primary">
+                  <a
+                    className="text-light text-decoration-none"
+                    href="https://lamontanarog.github.io/SplendoraCosmetics/"
+                  >
+                    Ver proyecto
+                  </a>
+                </Button>
+                &nbsp;
+                <Button className="mt-1" variant="primary">
+                  <a
+                    className="text-light text-decoration-none"
+                    href="https://github.com/lamontanarog/SplendoraCosmetics/"
+                  >
+                    Ver Repositorio
+                  </a>
+                </Button>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col">
+            <Card className="text-center" style={{ width: "19rem" }}>
+              <Card.Img variant="top" src="../icons/logoPodiatryLove.jpg" />
+              <Card.Body>
+                <Card.Title>Podiatry Love</Card.Title>
+                <Card.Text>
+                  Proyecto realizado con HTML, Css, Bootstrap, Sass.
+                </Card.Text>
+                <Button
+                  className="mx-4"
+                  onClick={() => {
+                    Swal.fire({
+                      title:
+                        "El proyecto consiste en el desarrollo de un sitio web informativo y de contacto para una médica podóloga. El sitio proporcionará información relevante sobre los servicios ofrecidos por la podóloga, así como detalles sobre su experiencia y formación profesional. Además, incluirá secciones informativas sobre diferentes condiciones podológicas, consejos de cuidado del pie y recomendaciones para mantener una buena salud podológica. La página de contacto permitirá a los usuarios ponerse en contacto con la médica podóloga para hacer consultas, programar citas o solicitar más información sobre los servicios ofrecidos.",
+                      width: 900,
+                      padding: "4em",
+                      color: "#fff",
+                      background: `url(https://plus.unsplash.com/premium_photo-1664373622112-00bc8a7925a3?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)
+                      no-repeat
+                      center
+                      `,
+                      backdrop: `
+                      rgba(0,0,123,0.4)
+                      url("../gifs/PodoGif.gif")
+                      left bottom
+                      no-repeat
+                    `,
+                    });
+                  }}
+                >
+                  Ver info
+                </Button>
+                &nbsp;
+                <Button variant="primary">
+                  <a
+                    className="text-light text-decoration-none"
+                    href="https://lamontanarog.github.io/PodiatryLove/"
+                  >
+                    Ver proyecto
+                  </a>
+                </Button>
+                &nbsp;
+                <Button className="mt-1" variant="primary">
+                  <a
+                    className="text-light text-decoration-none"
+                    href="https://github.com/lamontanarog/PodiatryLove/"
+                  >
+                    Ver Repositorio
+                  </a>
+                </Button>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col mt-5">
+            <Card className="text-center" style={{ width: "19rem" }}>
+              <Card.Img variant="top" src="../icons/iconoVintage.jpg" />
+              <Card.Body>
+                <Card.Title>Vintage Golden Show</Card.Title>
+                <Card.Text>
+                Este sitio web está construido utilizando ReactJS y Firebase como tecnologías principales.
+                </Card.Text>
+                <Button
+                  className="mx-4"
                   onClick={() => {
                     Swal.fire({
                       title:
@@ -35,84 +192,7 @@ function MisProyectos() {
                 >
                   Ver info
                 </Button>
-                {/* &nbsp;
-                <Button variant="primary">
-                  <a className='text-light text-decoration-none'  href="https://control-gastos-gold.vercel.app/">Ver proyecto</a>
-                </Button>
                 &nbsp;
-                <Button className="mx- mt-1" variant='primary'>
-                  <a className='text-light text-decoration-none' href="https://github.com/lamontanarog/controlGastos" >Ver Repositorio</a>
-                </Button> */}
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col">
-            <Card className="text-center" style={{ width: "19rem" }}>
-              <Card.Img
-                variant="top"
-                src="../icons/Splendora_Cosmetics-logos.jpeg"
-              />
-              <Card.Body>
-                <Card.Title>Splendora Cosmetics</Card.Title>
-                <Card.Text>
-                  Proyecto realizado con HTML, css, boostrap, JavaScript, apis
-                </Card.Text>
-                <Button variant="primary">
-                  <a
-                    className="text-light text-decoration-none"
-                    href="https://lamontanarog.github.io/SplendoraCosmetics/"
-                  >
-                    Ver proyecto
-                  </a>
-                </Button>
-                &nbsp;
-                <Button variant="primary">
-                  <a
-                    className="text-light text-decoration-none"
-                    href="https://github.com/lamontanarog/SplendoraCosmetics/"
-                  >
-                    Ver Repositorio
-                  </a>
-                </Button>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col">
-            <Card className="text-center" style={{ width: "19rem" }}>
-              <Card.Img variant="top" src="../icons/logoPodiatryLove.jpg" />
-              <Card.Body>
-                <Card.Title>Podiatry Love</Card.Title>
-                <Card.Text>
-                  Proyecto realizado con HTML, css, boostrap, JavaScript, apis
-                </Card.Text>
-                <Button variant="primary">
-                  <a
-                    className="text-light text-decoration-none"
-                    href="https://lamontanarog.github.io/PodiatryLove/"
-                  >
-                    Ver proyecto
-                  </a>
-                </Button>
-                &nbsp;
-                <Button variant="primary">
-                  <a
-                    className="text-light text-decoration-none"
-                    href="https://github.com/lamontanarog/PodiatryLove/"
-                  >
-                    Ver Repositorio
-                  </a>
-                </Button>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col mt-5">
-            <Card className="text-center" style={{ width: "19rem" }}>
-              <Card.Img variant="top" src="../icons/iconoVintage.jpg" />
-              <Card.Body>
-                <Card.Title>Vintage Golden Show</Card.Title>
-                <Card.Text>
-                  Proyecto Ecommers, consumiendo apis, utilizando ReactJs y firebase
-                </Card.Text>
                 <Button variant="primary">
                   <a
                     className="text-light text-decoration-none"
@@ -122,7 +202,7 @@ function MisProyectos() {
                   </a>
                 </Button>
                 &nbsp;
-                <Button variant="primary">
+                <Button className="mt-1" variant="primary">
                   <a
                     className="text-light text-decoration-none"
                     href="https://github.com/lamontanarog/Lamontanaro-vintage-golden-show"
--- src/Components/Sobremi.jsx
+++ src/Components/Sobremi.jsx
@@ -1,49 +1,102 @@
+import "./proyectos.css";
 
 function Sobremi() {
   return (
-    <div className="container">
-      <div id="carrusel01" className="carousel slide" data-bs-ride="carousel">
-        <div className="carousel-indicators">
-          <button type="button" data-bs-target="#carrusel01" data-bs-slide-to="0" className="active"></button>
-          <button type="button" data-bs-target="#carrusel01" data-bs-slide-to="1" ></button>
-          <button type="button" data-bs-target="#carrusel01" data-bs-slide-to="2" ></button>
-          <button type="button" data-bs-target="#carrusel01" data-bs-slide-to="3" ></button>
-        </div>
-
-        <div className="carousel-inner">
-          <div className="carousel-item active"> 
-          <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzN5N2o1eGZleXRuZWVoMjlhemN6MnNjdmc2d3B0NTh1ZGJlbzJuYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/EMzzgNQ6Yk3MQLdwdW/giphy.gif" alt="" className="d-block w-100" />
-          <div className="carousel-caption">
-            <h3 className="text-body">Hola</h3>
-          </div>
-          </div>
-
-          <div className="carousel-item">
-            <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzN5N2o1eGZleXRuZWVoMjlhemN6MnNjdmc2d3B0NTh1ZGJlbzJuYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/EMzzgNQ6Yk3MQLdwdW/giphy.gif" alt="" />
-            <div className="carousel-caption">
-              <h3> Yop</h3>
+    <div id="contenedor-sobre-mi" className="container mt-5">
+      <div
+        id="carouselExampleSlidesOnly"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner text-center">
+          <div className="carousel-item active">
+            <img
+              className="d-block w-100"
+              src="../public/img/fotocv.jpg"
+              alt="..."
+            />
+            <div className="bg-text-carusel mt-2">
+              <h1>Quien soy?</h1>
+              <h5>
+                Mi nombre es Giuliana Lamontanaro, tengo 23 años y cuento con
+                una sólida orientación hacia el desarrollo web desde hace casi 4
+                años. <strong>Actualmente, me encuentro culminando el último tramo de mi
+                carrera universitaria en programación en la UTN-FRT. </strong>
+              </h5>
             </div>
           </div>
-
           <div className="carousel-item">
-            <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzN5N2o1eGZleXRuZWVoMjlhemN6MnNjdmc2d3B0NTh1ZGJlbzJuYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/EMzzgNQ6Yk3MQLdwdW/giphy.gif" alt="" />
-            <div className="carousel-caption">
-              <h3> Hi</h3>
+            <img
+              className="d-block w-100"
+              src="../public/img/certificadoDW.png"
+              alt="..."
+            />
+            <div className="bg-text-carusel mt-2">
+              <h1>Desarrollo Web</h1>
+              <h5>
+                Cuento con amplios conocimientos en el desarrollo web, abarcando
+                áreas como el maquetado con HTML5, CSS3 y JavaScript. Además,
+                poseo experiencia en el uso de herramientas como Bootstrap y
+                Sass para la creación de interfaces web modernas y responsivas.
+              </h5>
             </div>
           </div>
-
+          <div className="carousel-item">
+            <img
+              className="d-block w-100"
+              src="../public/img/certificadoJs.png"
+              alt="..."
+            />
+            <div className="bg-text-carusel mt-2">
+              <h1>JavaScript</h1>
+              <h5>
+                También poseo un sólido entendimiento de los fundamentos de la
+                programación con JavaScript, así como de sus principios básicos.
+                Además, tengo experiencia en el uso de Git para el control de
+                versiones del código, lo que me permite colaborar de manera
+                efectiva en proyectos y mantener un flujo de trabajo organizado
+                y estructurado.
+              </h5>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              className="d-block w-100"
+              src="../public/img/certificadoReact.png"
+              alt="..."
+            />
+            <div className="bg-text-carusel mt-2">
+              <h1>React Js</h1>
+              <h5>
+                He obtenido certificación en ReactJS, lo que me ha permitido
+                familiarizarme con sus bibliotecas y hooks, los cuales optimizan
+                el proceso de creación de aplicaciones web mediante la
+                modularización de componentes. Además, poseo experiencia en la
+                gestión de rutas dentro de la aplicación, aplicando el concepto
+                de Single Page Application (SPA).
+              </h5>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              className="d-block w-100"
+              src="../public/img/certificadoCarrerafs.png"
+              alt="..."
+            />
+            <div className="bg-text-carusel mt-2">
+              <h1>Node Js, Express, MongoDB</h1>
+              <h5>
+                A pesar de haberme especializado como Desarrollador FrontEnd, en
+                la actualidad estoy ampliando mis habilidades para convertirme
+                en un Desarrollador FullStack, adquiriendo conocimientos en
+                tecnologías como Node.js con Express y MongoDB.
+              </h5>
+            </div>
+          </div>
         </div>
-
-        <button className="carousel-control-prev" type="button" data-bs-target="#carrusel01" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon"></span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carrusel01" data-bs-slide="next">
-          <span className="carousel-control-next-icon"></span>
-        </button>
-
       </div>
     </div>
-  )
+  );
 }
 
-export default Sobremi
+export default Sobremi;
