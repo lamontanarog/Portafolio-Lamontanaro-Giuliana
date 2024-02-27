10b647e766b3d5b75ee27f0b8cb05c34b1f5d739
Giuliana Lamontanaro <glamontanaro@gmail.com>
2024-02-27 18:17:27 -0300

    cambios en componente conocimientos

--- src/Components/Misconocimientos.jsx
+++ src/Components/Misconocimientos.jsx
@@ -1,7 +1,25 @@
+import './conocimientos.css'
 
 function Misconocimientos() {
   return (
-    <div>Misconocimientos</div>
+    <div>
+      <main>
+      <h1> Skills</h1>
+      <div className='lista-logos'>
+        <ul>
+          <li> <img src="../logosSkills/html.png" alt="Logo JavaScript" /></li>
+          <li><img src="../logosSkills/css.png" alt="" /></li>
+          <li><img src="../logosSkills/javascript.png" alt="" /></li>
+          <li><img src="../logosSkills/reactjs.png" alt="" /></li>
+          <li><img src="../logosSkills/vite.png" alt="" /></li>
+          <li><img src="../logosSkills/git.png" alt="" /></li>
+          <li><img src="../logosSkills/github.png" alt="" /></li>
+          <li><img src="./logosSkills/trello.png" alt="" /></li>
+        </ul>
+      </div>
+      <h2>conocimientos</h2>
+      </main>
+    </div>
   )
 }
 
